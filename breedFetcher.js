fdbf22f4f4579dac9c17398a461d8d6b80ee77d4
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-29 14:37:01 +0000

    breedFetcher update

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,36 @@
-const request = require('request'); //any path way problems
+// const request = require('request'); //any path way problems
 
-const breedName = 'Siberian';
+// const breedName = 'Siberian';
+// const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.log('Error occurred: ', error);
+//     return;
+//   }
+
+//   const data = JSON.parse(body);
+//   console.log(data);
+//   console.log(typeof body);
+//   console.log(typeof data);
+  
+// });
+
+const request = require('request');
+const breedName = process.argv[2];
+
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 request(url, (error, response, body) => {
   if (error) {
-    console.log('Error occurred: ', error);
-    return;
+    console.log(`Error fetching breed information: ${error}`);
+    process.exit();
   }
-
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  if (data.length === 0) {
+    console.log(`Breed "${breedName}" not found`);
+    process.exit();  
+    // return;
+  }
+  console.log(data[0].description);
 });
