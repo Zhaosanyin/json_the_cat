e1d53c4ed96ce1d45004e428c339e3eab834b0e7
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-29 16:15:48 +0000

    week5 testing cats as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -16,21 +16,42 @@
   
 // });
 
+// const request = require('request');
+//  const breedName = process.argv[2];
+
+// const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.log(`Error fetching breed information: ${error}`);
+//     process.exit();
+//   }
+//   const data = JSON.parse(body);
+//   if (data.length === 0) {
+//     console.log(`Breed "${breedName}" not found`);
+//     process.exit();
+//     // return;
+//   }
+//   console.log(data[0].description);
+// });
+
 const request = require('request');
-const breedName = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(`Error fetching breed information: ${error}`, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(`Breed "${breedName}" not found`, null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(`Error fetching breed information: ${error}`);
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`Breed "${breedName}" not found`);
-    process.exit();
-    // return;
-  }
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
+
